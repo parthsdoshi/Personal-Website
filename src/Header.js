e60fc7ae3bdec4d3f42eee510e70b53d21d43cba
Parth Doshi <doshi.parth9@gmail.com>
2018-09-16 20:22:20 -0400

    added email modal

--- src/Header.js
+++ src/Header.js
@@ -23,12 +23,15 @@ class Header extends Component {
 
                                     <div className="content subtitle" style={this.topPadding}>
                                         <p>I'm a junior at <span className="has-text-weight-semibold">Purdue University</span> studying <a href="https://www.cs.purdue.edu/undergraduate/curriculum/track_MI.html" className="has-text-link">Computer Science</a> and <a href="http://catalog.purdue.edu/preview_program.php?catoid=9&poid=10329" className="has-text-link">Mathematical Statistics</a>.</p>
-                                        <p>In the past, I've explored many different areas of Computer Science trying to find what I really enjoy. So far I've tried my hand at the following: <span className="has-text-weight-semibold">frontend development, networking, systems, and most recently, data analysis</span>.</p>
-                                        <p>
+                                        <p>In the past, I've explored many different areas of Computer Science trying to find what I enjoy. So far I've tried my hand at the following: <span className="has-text-weight-semibold">frontend development, networking, systems, and most recently, data analysis</span>.</p>
+                                        {/*<p>
                                             Going forward, I'd like to steer my career towards machine learning and its applications in the field. Towards this goal, I have selected the "Machine Intelligence" track and have picked up a Mathematical Statistics major.
+                                        </p>*/}
+                                        <p>
+                                            Aside from my professional life, I ballroom dance competitively and listen to a large amount of music.
                                         </p>
                                         <p>
-                                            If you're interested, take a look at my resume!
+                                            If you're interested, there's a lot more information on my resume!
                                         </p>
                                     </div>
 
--- src/Navbar.js
+++ src/Navbar.js
@@ -7,8 +7,9 @@ class Navbar extends Component {
         super(props);
 
         this.navbarBurger = React.createRef()
+        this.emailModal = React.createRef()
 
-        this.addBurgerEvent = this.addBurgerEvent.bind(this)
+        this.email = "doshi.parth9@gmail.com"
     }
 
     componentDidMount() {
@@ -16,6 +17,41 @@ class Navbar extends Component {
             this.addBurgerEvent(this.navbarBurger.current)
         }
     }
+
+    openEmailModal = () => {
+        this.emailModal.current.classList.toggle("is-active")
+    }
+    
+    closeEmailModal = () => {
+        this.emailModal.current.classList.toggle("is-active")
+    }
+
+    copyEmailToClipboard = () => {
+        this.copyToClipboard(this.email)
+    }
+
+    // taken from online
+    // all it does is creates a textarea with what you want to copy and then calls the appropriate api
+    // then it removes the textarea
+    copyToClipboard = (str) => {
+        const el = document.createElement('textarea');  // Create a <textarea> element
+        el.value = str;                                 // Set its value to the string that you want copied
+        el.setAttribute('readonly', '');                // Make it readonly to be tamper-proof
+        el.style.position = 'absolute';                 
+        el.style.left = '-9999px';                      // Move outside the screen to make it invisible
+        document.body.appendChild(el);                  // Append the <textarea> element to the HTML document
+        const selected =            
+            document.getSelection().rangeCount > 0        // Check if there is any content selected previously
+            ? document.getSelection().getRangeAt(0)     // Store selection if found
+            : false;                                    // Mark as false to know no selection existed before
+        el.select();                                    // Select the <textarea> content
+        document.execCommand('copy');                   // Copy - only works as a result of a user action (e.g. click events)
+        document.body.removeChild(el);                  // Remove the <textarea> element
+        if (selected) {                                 // If a selection existed before copying
+            document.getSelection().removeAllRanges();    // Unselect everything on the HTML document
+            document.getSelection().addRange(selected);   // Restore the original selection
+        }
+    };
 
     render() {
 
@@ -52,40 +88,58 @@ class Navbar extends Component {
                             <div className="navbar-end">
                                 {/*<a className="navbar-item">
                                     Work Experience
-                                </a>*/}
+                            </a>*/}
 
-                                <div className={"navbar-item is-hoverable " + (this.props.isFooter ? "has-dropdown-up" : "has-dropdown")}>
-                                    <a className="navbar-link">
-                                        Projects
+                            <div className={"navbar-item is-hoverable " + (this.props.isFooter ? "has-dropdown-up" : "has-dropdown")}>
+                                <a className="navbar-link">
+                                    Active Projects
+                                </a>
+                                <div className="navbar-dropdown is-boxed">
+                                    <a className="navbar-item" href="http://mediaq.parthdoshi.com">
+                                        MediaQ
                                     </a>
-                                    <div className="navbar-dropdown is-boxed">
-                                        <a className="navbar-item" href="http://mediaq.parthdoshi.com">
-                                            MediaQ
-                                        </a>
-                                        <hr className="navbar-divider" />
-                                        <div className="navbar-item">
-                                            More to come soon :)
-                                        </div>
+                                    <hr className="navbar-divider" />
+                                    <div className="navbar-item">
+                                        More to come soon :)
                                     </div>
                                 </div>
+                            </div>
 
-                                <a className="navbar-item" href="https://github.com/parthsdoshi">
-                                    <div className="button is-link is-rounded is-outlined">
-                                        Github
-                                        <span className="icon" style={marginLeftStyle}>
-                                            <i className="fab fa-github"></i>
-                                        </span>
+                            {/*<div className={"navbar-item is-hoverable " + (this.props.isFooter ? "has-dropdown-up" : "has-dropdown")}>
+                                <a className="navbar-link">
+                                        External Links
+                                    </a>
+                                    <div className="navbar-dropdown is-boxed">
+                                        <a className="navbar-item" href="https://github.com/parthsdoshi">
+                                            Github
+                                        </a>
+                                        <a className="navbar-item" href="https://linkedin.com/in/parthsdoshi">
+                                            LinkedIn
+                                        </a>
+                                        <hr className="navbar-divider" />
+                                        <a className="navbar-item" href="mailto:doshi.parth9@gmail.com">
+                                            Email Me
+                                        </a>
                                     </div>
-                                </a>
-                                <a className="navbar-item" href="https://linkedin.com/in/parthsdoshi">
-                                    <div className="button is-link is-rounded is-outlined">
-                                        LinkedIn
+                            </div>*/}
+
+                            <a className="navbar-item" href="https://github.com/parthsdoshi">
+                                <div className="button is-link is-rounded is-outlined">
+                                    Github
+                                    <span className="icon" style={marginLeftStyle}>
+                                        <i className="fab fa-github"></i>
+                                    </span>
+                                </div>
+                            </a>
+                            <a className="navbar-item" href="https://linkedin.com/in/parthsdoshi">
+                                <div className="button is-link is-rounded is-outlined">
+                                    LinkedIn
                                         <span className="icon" style={marginLeftStyle}>
                                             <i className="fab fa-linkedin"></i>
                                         </span>
                                     </div>
                                 </a>
-                                <a className="navbar-item" href="mailto:doshi.parth9@gmail.com">
+                                <a className="navbar-item" onClick={this.openEmailModal}>
                                     <div className="button is-link is-rounded is-outlined">
                                         Email
                                         <span className="icon" style={marginLeftStyle}>
@@ -105,11 +159,38 @@ class Navbar extends Component {
                         </div>
                     </div>
                 </nav>
+                <div className="modal" id="emailModal" ref={this.emailModal}>
+                    <div className="modal-background"></div>
+                    <div className="modal-card">
+                        <header className="modal-card-head">
+                            <p className="modal-card-title">Email</p>
+                            <button className="delete" aria-label="close" onClick={this.closeEmailModal}></button>
+                        </header>
+                        <section className="modal-card-body">
+                            <h4 className="is-4 subtitle">
+                                <a href={"mailto:" + this.email}>
+                                    {this.email}
+                                </a>
+                            </h4>
+                        </section>
+                        <footer className="modal-card-foot">
+                            <button className="button is-dark" onClick={this.copyEmailToClipboard}>
+                                Copy to clipboard
+                            </button>
+                            <a className="button is-link" href={"mailto:" + this.email}>
+                                Open externally
+                            </a>
+                            <button className="button" onClick={this.closeEmailModal}>
+                                Done
+                            </button>
+                        </footer>
+                    </div>
+                </div>
             </div>
         )
     }
 
-    addBurgerEvent(e) {
+    addBurgerEvent = (e) => {
         // Add a click event on each of them
         e.addEventListener('click', () => {
 
