df2e82f3c314b5911595956b4fc2a18b24e216c0
Parth Doshi <doshi.parth9@gmail.com>
2019-09-20 12:09:02 -0400

    Update resume.js

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -4,9 +4,9 @@ import { graphql } from "gatsby";
 
 const Resume = ({ data }) => (
     <iframe src={data.site.siteMetadata.resume} style={{position: "fixed", top: 0, left: 0, bottom: 0, right: 0, width: "100%", height: "100%", border: "none", margin: 0, padding: 0, overflow: "hidden", zIndex:999999}} title="resume">
-        Your browser doesn't support iframes.
-        You can find the resume here:
-        <a href={data.site.siteMetadata.resume}>Resume</a>
+        <a href={data.site.siteMetadata.resume}>
+            Parth Doshi's very interesting résumé. You're seeing this text because you probably don't have javascript enabled!
+        </a>
     </iframe>
 )
 
