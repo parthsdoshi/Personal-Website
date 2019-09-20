996a35364a20b6fa3565526934bd541cfdff3965
Ubuntu <parth@website.elci13hnh0wubej51op2rp0y3d.bx.internal.cloudapp.net>
2019-09-20 19:27:55 +0000

    fix seo ref

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react"
 
 import { graphql } from "gatsby";
 import NavbarFooter from "../components/NavbarFooter";
+import SEO from "../components/seo";
 
 const Resume = ({ data }) => {
     const [height, setHeight] = useState("100%")
