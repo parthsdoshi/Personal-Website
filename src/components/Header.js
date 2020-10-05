9be9938eeed1a0b095592372f02e4de352f9b613
Parth Doshi <doshi.parth9@gmail.com>
2020-10-05 01:32:44 -0700

    add tina cms to website

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,10 @@
 import React from 'react';
 
 import { Link, useStaticQuery, graphql } from 'gatsby';
+import { useRemarkForm } from 'gatsby-tinacms-remark';
+import { usePlugin } from 'tinacms';
+import { InlineForm, InlineTextField, InlineTextarea } from 'react-tinacms-inline';
+import { InlineWysiwyg } from 'react-tinacms-editor';
 
 import ProfilePicture from './ProfilePhoto';
 
@@ -10,46 +14,51 @@ const Header = () => {
         {
             file(base: {eq: "header.md"}) {
                 childMarkdownRemark {
-                    html
                     frontmatter {
                         title
                         date
                     }
+                    ...TinaRemark
                 }
             }
         }
     `)
-    const headerData = data.file.childMarkdownRemark
+    const [_headerData, form] = useRemarkForm(data.file.childMarkdownRemark)
+    usePlugin(form)
 
     let topPadding = {paddingTop: "1.5em"}
 
     return (
-        <section className="hero">
-            <div className="hero-body">
-                <div className="container">
-                    <div className="columns box is-vcentered">
-                        <div className="column is-half">
-                            <figure className="image">
-                                <ProfilePicture imgStyle={{borderRadius: "1%"}} />
-                            </figure>
-                        </div>
-                        <div className="column is-half">
-                            <div className="has-text-centered">
-                                <div className="content title">
-                                    <p>{headerData.frontmatter.title}</p>
-                                </div>
-                                <div className="content subtitle" style={topPadding} dangerouslySetInnerHTML={{__html: headerData.html}} />
-                                <div style={topPadding}>
-                                    <Link className="button is-link" to="/resume">
-                                        Résumé
-                                    </Link>
+        <InlineForm form={form}>
+            <section className="hero">
+                <div className="hero-body">
+                    <div className="container">
+                        <div className="columns box is-vcentered">
+                            <div className="column is-half">
+                                <figure className="image">
+                                    <ProfilePicture imgStyle={{borderRadius: "1%"}} />
+                                </figure>
+                            </div>
+                            <div className="column is-half">
+                                <div className="has-text-centered">
+                                    <div className="content title">
+                                        <InlineTextarea name="rawFrontmatter.title" />
+                                    </div>
+                                    <div className="content subtitle" style={topPadding}>
+                                        <InlineWysiwyg name="rawMarkdownBody" />
+                                    </div>
+                                    <div style={topPadding}>
+                                        <Link className="button is-link" to="/resume">
+                                            Résumé
+                                        </Link>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        </section>
+            </section>
+        </InlineForm>
     )
 }
 
